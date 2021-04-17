5051b8646da146bbdcf001f5f91fe9000609dd08
sea-men <79608008118@yandex.ru>
2021-04-17 14:17:18 +0500

    18: new page Dialogs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import h from './Header.module.css';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className={h.header}>
+    return <header className={s.header}>
         <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftwistedchoppers.com%2Fwp-content%2Fuploads%2F2012%2F04%2FTC-LOGO-WHITE.jpg&f=1&nofb=1' />
     </header>
 }
